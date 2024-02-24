ca32de17ba13633a74a50dc696529da4420457bf
vinaysuyal <vinaysuyal2012@gmail.com>
2024-02-24 13:10:22 +0530

    Header added, Enter key differentiation added

--- src/Pages/DiffCheck.js
+++ src/Pages/DiffCheck.js
@@ -1,11 +1,13 @@
 import { Fragment, useState } from "react";
 import { analyseDifferences } from "../Utils/util";
+import Header from "../Component/Header";
 
 function DiffChecker() {
   const [outputView, setOutputView] = useState("Line");
   const [original, setOriginal] = useState("");
   const [modified, setModified] = useState("");
   const [differences, setDifferences] = useState([]);
+  const [softWrap, setSoftWrap] = useState(true);
 
   function processTextarea(text) {
     return text.replace(/\r\n/g, "\n");
@@ -28,9 +30,10 @@ function DiffChecker() {
 
   return (
     <>
+      <Header />
       <div className="input">
         <div className="input-container-original">
-          <label htmlFor="originalTextarea">Original Text:</label>
+          <label htmlFor="originalTextarea">Original</label>
           <textarea
             onChange={(e) => {
               setOriginal(processTextarea(e.target.value));
@@ -40,7 +43,7 @@ function DiffChecker() {
           ></textarea>
         </div>
         <div className="input-container-modified">
-          <label htmlFor="modifiedTextarea">Modified Text:</label>
+          <label htmlFor="modifiedTextarea">Modified</label>
           <textarea
             onChange={(e) => {
               setModified(processTextarea(e.target.value));
@@ -50,21 +53,25 @@ function DiffChecker() {
           ></textarea>
         </div>
       </div>
-      <span>
-        <label htmlFor="view">Choose View:</label>
-        <select
-          onChange={handleViewChange}
-          value={outputView}
-          id="view"
-          name="view"
-        >
-          <option value="Character">Character</option>
-          <option value="Line">Line View</option>
-        </select>
-        <br />
-        <button onClick={handleShowDifference}>Show Difference(s)</button>
-      </span>
-
+      <div className="controls">
+        <span>
+          <div style={{ padding: "2px" }}>
+            <label style={{ display: "inline" }} htmlFor="view">
+              Choose View:
+            </label>
+            <select
+              onChange={handleViewChange}
+              value={outputView}
+              id="view"
+              name="view"
+            >
+              <option value="Character">Character</option>
+              <option value="Line">Line View</option>
+            </select>
+          </div>
+          <button onClick={handleShowDifference}>Show Difference(s)</button>
+        </span>
+      </div>
       {differences.length > 0 && (
         <>
           <div className="legend">
@@ -72,7 +79,9 @@ function DiffChecker() {
               <span
                 className="legend-color"
                 style={{ backgroundColor: "#FFD2D2" }}
-              ></span>
+              >
+                {""}
+              </span>
               <span>Removed</span>
             </div>
             <div className="legend-item">
@@ -85,20 +94,50 @@ function DiffChecker() {
             <div className="legend-item">
               <span
                 className="legend-color"
-                style={{ backgroundColor: "#FFFFFF", border: "1px solid #000" }}
+                style={{ backgroundColor: "#FFFFFF" }}
               ></span>
               <span>Common</span>
             </div>
+            <div className="legend-item">
+              <span
+                className="legend-color"
+                style={{ backgroundColor: "#FFD2D2" }}
+              >
+                {"⏎"}
+              </span>
+              <span>Removed New Line</span>
+            </div>
+            <div className="legend-item">
+              <span
+                className="legend-color"
+                style={{ backgroundColor: "#D2FFD2" }}
+              >
+                {"⏎"}
+              </span>
+              <span>Added New Line</span>
+            </div>
           </div>
           <div className="output">
-            <div>
-              {differences.map((obj) => {
-                const splittedObj = obj.value.split("\n");
-                return splittedObj.map((word, index) => (
+            <button
+              onClick={() => {
+                setSoftWrap((prev) => !prev);
+              }}
+              className={`output__wrap-toggle ${
+                softWrap
+                  ? "output__wrap-toggle__on"
+                  : "output__wrap-toggle__off"
+              }`}
+            >
+              Soft wrap
+            </button>
+            <div style={{ whiteSpace: softWrap ? "pre-wrap" : "pre" }}>
+              {differences.map((obj, index) => {
+                let val = obj.value;
+                if (obj.type !== "common") val = val.replace("\n", " ⏎ \n");
+                return (
                   <Fragment key={index}>
                     <span
                       style={{
-                        whiteSpace: "pre",
                         backgroundColor:
                           obj.type === "added"
                             ? "#D2FFD2"
@@ -107,21 +146,17 @@ function DiffChecker() {
                             : "none",
                       }}
                     >
-                      {word}
+                      {val}
                     </span>
                     {outputView === "Line" && <br />}
-                    {outputView !== "Line" &&
-                      index !== splittedObj.length - 1 && <br />}
-                    {outputView !== "Line" &&
-                      index === splittedObj.length - 1 &&
-                      splittedObj[index].endsWith("\n") && <br />}
                   </Fragment>
-                ));
+                );
               })}
             </div>
           </div>
         </>
       )}
+      <div className="footer"></div>
     </>
   );
 }
